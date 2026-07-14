8eef42d54f47297c857c26c89e5fd67b606a6155
thondamvinodkumar-wq <thondamvinodkumar@gmail.com>
2026-07-14 06:49:18 +0530

    Update paymentRoutes.js

--- paymentRoutes.js
+++ paymentRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { handleRazorpayWebhook } = require('../razorpayWebhook');
+const { handleRazorpayWebhook } = require('./razorpayWebhook');
 
 router.post('/razorpay-webhook', handleRazorpayWebhook);
 
